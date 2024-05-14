65ddf1259ae4de2788d79fb8c4002d872c4de1da
Philip Kvarnhäll <phikva0412@edu.halmstad.se>
2024-05-14 15:52:13 +0200

    Lucide svelte icon library inc

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -4,7 +4,7 @@
 
 
 <header class="flex">
-    <Button class="flex w-20 h-15 m-2" variant="outline">></Button>
+    <Button class="flex w-20 h-15 m-2" variant="outline">Lucide svelte icon library here</Button>
     <h2 class="text-center flex-1 text-4xl">Maskiner</h2>
 
 
