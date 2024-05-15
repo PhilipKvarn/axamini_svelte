59c919baa4d3748fea0ac37ce754481b2a145a7d
Philip Kvarnhäll <phikva0412@edu.halmstad.se>
2024-05-15 14:41:02 +0200

    feat: Added a site selection screen as menu

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,12 +1,47 @@
 <script>
   import {Button} from "$lib/components/ui/button/index.js";
+  import * as Sheet from "$lib/components/ui/sheet/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
+  import { Menu } from 'lucide-svelte';
 </script>
 
 
 <header class="flex">
-    <Button class="flex w-20 h-15 m-2" variant="outline">Lucide svelte icon library here</Button>
-    <h2 class="text-center flex-1 text-4xl">Maskiner</h2>
 
+  <Sheet.Root>
+    <Sheet.Trigger asChild let:builder>
+      <Button builders={[builder]} class="flex w-[80px] h-[60px] m-5" variant="outline"><Menu strokeWidth={2.5}/></Button>
+    </Sheet.Trigger>
+    <Sheet.Content  class="bg-blue-500" side="left">
+      <Sheet.Header>
+        <Sheet.Title><Label class="text-3xl text-white">Menu</Label></Sheet.Title>
+        <Sheet.Description class="text-white">
+          Change Site
+        </Sheet.Description>
+      </Sheet.Header>
+      <div class="grid gap-4 py-4">
+        <div class="items-center">
+          <Button variant="link" class="w-full text-2xl text-white font-semibold">Index</Button>
+        </div>
+        <div class="items-center">
+          <Button variant="link" class="w-full text-2xl text-white font-semibold">Machines</Button>
+        </div>
+        <div class="items-center">
+          <Button variant="link"  class="w-full text-2xl text-white font-semibold">Tasks</Button>
+        </div>
+        <div class="items-center">
+          <Button variant="link"  class="w-full text-2xl text-white font-semibold">Users</Button>
+        </div>
+      </div>
+      <Sheet.Footer>
+        <!-- <Label class="text-xl text-white">AXAMINI CORPORATION</Label> -->
+      </Sheet.Footer>
+    </Sheet.Content>
+  </Sheet.Root>  
+  
+  <Label class="text-center flex-1 text-4xl text-white font-light m-auto">Maskiner</Label>
+
+    
 
 </header>
 
@@ -14,6 +49,6 @@
 
     header{
     min-height: 10vh;
-    background-color: #eee;
+    background-color: #488eff;
     }
 </style>
--- src/lib/components/machine-table.svelte
+++ src/lib/components/machine-table.svelte
@@ -3,6 +3,7 @@
     import * as Table from "$lib/components/ui/table";
     import { machines } from "$lib/stores";
     import { onMount } from 'svelte';
+    import * as Dialog from "$lib/components/ui/dialog/index.js";
 
     onMount(async () => {
     try {
@@ -17,11 +18,20 @@
     }
   });
 
+  function showMachineCard(id){
+    const machine = $machines.find(machine => machine.id === id);
+    if (machine) {
+      machines.set(data)
+    } else {
+      console.error(`Machine with ID ${id} not found.`);
+    }
+  }
+
 </script>
 
 <div>
     <Table.Root>
-        <Table.Caption>A list of your recent invoices.</Table.Caption>
+        <Table.Caption>Lista av alla maskiner.</Table.Caption>
         <Table.Header>
           <Table.Row>
             <Table.Head class="w-[100px]">id</Table.Head>
@@ -33,7 +43,7 @@
         </Table.Header>
         <Table.Body>
             {#each $machines as machine }
-            <Table.Row>
+              <Table.Row on:click={() => showMachineCard(machine.id)}>
                 <Table.Cell class="font-medium">{machine.id}</Table.Cell>
                 <Table.Cell><a href="">{machine.name}</a></Table.Cell>
                 <Table.Cell>{machine.mechanic_id}</Table.Cell>
@@ -43,6 +53,32 @@
             {/each}
         </Table.Body>
     </Table.Root>
+
+    <Dialog.Root>
+      <Dialog.Trigger>Edit Profile</Dialog.Trigger>
+      <Dialog.Content class="sm:max-w-[425px]">
+        <Dialog.Header>
+          <Dialog.Title>Edit Machine</Dialog.Title>
+          <Dialog.Description>
+            Make changes to your profile here. Click save when you're done.
+          </Dialog.Description>
+        </Dialog.Header>
+        <div class="grid gap-4 py-4">
+          <div class="grid grid-cols-4 items-center gap-4">
+            <!-- <Label for="name" class="text-right">Name</Label>
+            <Input id="name" value="Pedro Duarte" class="col-span-3" /> -->
+          </div>
+          <div class="grid grid-cols-4 items-center gap-4">
+            <!-- <Label for="username" class="text-right">Username</Label>
+            <Input id="username" value="@peduarte" class="col-span-3" /> -->
+          </div>
+        </div>
+        <Dialog.Footer>
+          <!-- <Button type="submit">Save changes</Button> -->
+        </Dialog.Footer>
+      </Dialog.Content>
+    </Dialog.Root>
+
 </div>
 
 
