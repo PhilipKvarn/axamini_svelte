df9f392615925dfa93f4dee4bef5b2a6aa7425ca
PhilipKvarn <philip20ph@gmail.com>
2024-05-19 22:57:55 +0200

    added user table form

---
+++ src/lib/components/user-dropdown.svelte
@@ -0,0 +1,153 @@
+<script>
+    import { tasks } from "$lib/stores";
+    import {Ellipsis} from 'lucide-svelte';
+    
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+    import {Button} from "$lib/components/ui/button/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import * as Dialog from "$lib/components/ui/dialog/index.js";
+    import * as AlertDialog from "$lib/components/ui/alert-dialog";
+
+
+    export let task;
+    let dialogOpen = false;
+    let alertOpen = false;
+
+
+    //const machine = $machines.find(machine => machine.id == machineId);
+
+    async function deleteMachine(id){
+
+      try {
+            const response = await fetch('http://localhost:1738/task',{
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({"id":id}),
+            });
+            console.log(response)
+            if(response.status != 200){
+                throw error
+            }
+
+            tasks.update((currentTasks) => {
+              return currentTasks.filter(task => task.id !== id);
+            });
+
+        } catch (error) {
+            console.log(error)
+            console.log("Couldn't create new task")
+        }
+    }
+
+    async function updateTask(updatedTask){
+
+      try {
+            const response = await fetch('http://localhost:1738/task',{
+            method: 'Put',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedTask),
+            });
+            console.log(response)
+            if(response.status != 200){
+                throw error
+            }
+
+            tasks.update(currentTasks => {
+              return currentTasks.map(task => {
+                if (task.id === updatedTask.id) {
+                  return { ...task, ...updatedTask };
+                }
+                return task;
+              });
+            });
+        console.log("UPDATE: " + tasks)
+
+        } catch (error) {
+            console.log(error)
+            console.log("Couldn't create new machine")
+        }
+    }
+    
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger asChild let:builder>
+		<Button
+			variant="ghost"
+			builders={[builder]}
+			class="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+		>
+			<Ellipsis class="h-4 w-4" />
+			<span class="sr-only">Open Menu</span>
+		</Button>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content class="w-[160px]" align="end">
+        <DropdownMenu.Item on:click={() => {
+            dialogOpen = true
+        }}>Edit Task</DropdownMenu.Item>
+		<DropdownMenu.Separator />
+		  <DropdownMenu.Item on:click={() => {
+        alertOpen = true
+      }}>Delete <DropdownMenu.Shortcut>⌘⌫</DropdownMenu.Shortcut> </DropdownMenu.Item>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
+
+<Dialog.Root bind:open={dialogOpen}>
+    <Dialog.Content class="sm:max-w-[425px]">
+      <Dialog.Header>
+        <Dialog.Title>Edit profile</Dialog.Title>
+        <Dialog.Description>
+          Make changes to your Task here, Click save to save changes.
+        </Dialog.Description>
+      </Dialog.Header>
+      <div class="grid gap-4 py-4">
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="name" class="text-right">Name</Label>
+          <Input id="name" bind:value={task.name} class="col-span-3" />
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="next_execution_date" class="text-right">Next Date</Label>
+          <Input id="next_execution_date" bind:value={task.next_execution_date} class="col-span-3" />
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="interval_days" class="text-right">Interval</Label>
+          <Input id="interval_days" bind:value={task.interval_days} class="col-span-3" />
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="execution_time" class="text-right">Time</Label>
+          <Input  id="execution_time" bind:value={task.execution_time} class="col-span-3" />
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="machine_id" class="text-right">maskin id</Label>
+          <Input  id="machine_id" bind:value={task.machine_id} class="col-span-3" />
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="description" class="text-right">info</Label>
+          <Input  id="description" bind:value={task.description} class="col-span-3" />
+        </div>
+      </div>
+      <Dialog.Footer>
+        <Button type="submit" on:click={() => updateTask(task)}>Save changes</Button>
+      </Dialog.Footer>
+    </Dialog.Content>
+</Dialog.Root>
+
+<AlertDialog.Root bind:open={alertOpen}>
+  <AlertDialog.Content>
+    <AlertDialog.Header>
+      <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+      <AlertDialog.Description>
+        This action cannot be undone. This will permanently delete your task and remove your it's from our servers.
+      </AlertDialog.Description>
+    </AlertDialog.Header>
+    <AlertDialog.Footer>
+      <AlertDialog.Cancel>CANCEL</AlertDialog.Cancel>
+      <AlertDialog.Action class="bg-red-500" on:click={() => deleteMachine(task.id)} >DELETE</AlertDialog.Action>
+    </AlertDialog.Footer>
+  </AlertDialog.Content>
+</AlertDialog.Root>
---
+++ src/lib/components/user-table.svelte
@@ -0,0 +1,96 @@
+
+<script>
+    import * as Table from "$lib/components/ui/table";
+    import { tasks } from "$lib/stores";
+    import { onMount } from 'svelte';
+    import * as Dialog from "$lib/components/ui/dialog/index.js";
+    import TaskDropdown from "./task-dropdown.svelte";
+    import MachineForm from "./machine-form.svelte";
+
+    onMount(async () => {
+    try {
+      const response = await fetch('http://localhost:1738/users');
+      if (response.status != 200) {
+        throw new Error(response.statusText);
+      }
+      const data = await response.json();
+      tasks.set(data);
+      console.log($tasks)
+    } catch (error) {
+      //  DUMMY TASKS NÄR MAN INTE HAR IGÅNG DATABASEN 
+      //  DUMMY TASKS NÄR MAN INTE HAR IGÅNG DATABASEN 
+      //  DUMMY TASKS NÄR MAN INTE HAR IGÅNG DATABASEN 
+      //  DUMMY TASKS NÄR MAN INTE HAR IGÅNG DATABASEN 
+      //  DUMMY TASKS NÄR MAN INTE HAR IGÅNG DATABASEN 
+      tasks.set([
+        { id: '1', name: 'Städa', next_execution_date: '24-08-12', interval_days: '7', execution_time: '120', machine_id:"1",description:"" },
+        { id: '2', name: 'Slipa', next_execution_date: '24-08-04', interval_days: '31', execution_time: '60', machine_id:"2",description:"" },
+        { id: '3', name: 'Betala Skatt', next_execution_date: '0', interval_days: '0', execution_time: '0', machine_id:"3",description:"Det finns ingen information att läsa här" },
+
+      ])
+      console.error('Fetch error: ', error);
+    }
+  });
+</script>
+
+<div>
+    <Table.Root>
+        <Table.Caption>Lista av alla maskiner.</Table.Caption>
+        <Table.Header>
+          <Table.Row>
+            <Table.Head class="w-[100px]">id</Table.Head>
+            <Table.Head>Name</Table.Head>
+            <Table.Head>Next Date</Table.Head>
+            <Table.Head class="text-right">Interval</Table.Head>
+            <Table.Head>Time</Table.Head>
+            <Table.Head>maskin id</Table.Head>
+            <Table.Head>info</Table.Head>
+            <Table.Head><MachineForm></MachineForm></Table.Head>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+            {#each $tasks as task }
+              <Table.Row>
+                <Table.Cell class="font-medium">{task.id}</Table.Cell>
+                <Table.Cell>{task.name}</Table.Cell>
+                <Table.Cell>{task.next_execution_date}</Table.Cell>
+                <Table.Cell>{task.interval_days}</Table.Cell>
+                <Table.Cell>{task.execution_time}</Table.Cell>
+                <Table.Cell>{task.machine_id}</Table.Cell>
+                <Table.Cell>{task.description}</Table.Cell>
+                <Table.Cell>
+                  <TaskDropdown {task}></TaskDropdown>
+                </Table.Cell>
+              </Table.Row>
+            {/each}
+        </Table.Body>
+    </Table.Root>
+
+<!--     <Dialog.Root>
+      <Dialog.Trigger>Edit Profile</Dialog.Trigger>
+      <Dialog.Content class="sm:max-w-[425px]">
+        <Dialog.Header>
+          <Dialog.Title>Edit Machine</Dialog.Title>
+          <Dialog.Description>
+            Make changes to your profile here. Click save when you're done.
+          </Dialog.Description>
+        </Dialog.Header>
+        <div class="grid gap-4 py-4">
+          <div class="grid grid-cols-4 items-center gap-4">
+            <Label for="name" class="text-right">Name</Label>
+            <Input id="name" value="Pedro Duarte" class="col-span-3" />
+          </div>
+          <div class="grid grid-cols-4 items-center gap-4">
+            <Label for="username" class="text-right">Username</Label>
+            <Input id="username" value="@peduarte" class="col-span-3" />
+          </div>
+        </div>
+        <Dialog.Footer>
+          <Button type="submit">Save changes</Button>
+        </Dialog.Footer>
+      </Dialog.Content>
+    </Dialog.Root> -->
+
+</div>
+
+
